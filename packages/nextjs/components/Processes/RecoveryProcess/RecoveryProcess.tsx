4725caa88c30dec865ea747829aec49f49114662
Fran <franciscopa91@gmail.com>
2023-08-28 22:39:14 +0200

    improve text for descriptive steps and modals

--- packages/nextjs/components/Processes/RecoveryProcess/RecoveryProcess.tsx
+++ packages/nextjs/components/Processes/RecoveryProcess/RecoveryProcess.tsx
@@ -28,14 +28,17 @@ export const RecoveryProcess = ({
   safeAddress,
   hackedAddress,
 }: IProps) => {
-  const {showError} = useShowError();
+  const { showError } = useShowError();
 
   if (recoveryStatus == RecoveryProcessStatus.INITIAL) {
     return <></>;
   }
 
   if (recoveryStatus == RecoveryProcessStatus.GAS_PAID) {
-    showError("you already covered the gas. If you're in a confussy situation, clear cookies and refresh page.");
+    showError(
+      "It appears that gas costs have been covered for this action. If you're encountering any confusion, consider clearing your cookies and refreshing the page.",
+    );
+     //TODO provide clear data mechanism title "Gas Covered Already"
     return;
   }
 
@@ -44,10 +47,11 @@ export const RecoveryProcess = ({
       <CustomPortal
         title={"Clear cache"}
         description={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti."
+          "We've encountered an issue due to outdated cached data. Please clear your app's cache to resolve this error."
         }
         image={VideoSvg}
       />
+        //TODO provide clear data mechanism
     );
   }
 
@@ -56,7 +60,7 @@ export const RecoveryProcess = ({
       <CustomPortal
         title={"Switch to safe address"}
         description={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti."
+          "To ensure a secure asset transfer, switch to your safe wallet. This step guarantees the safe retrieval of your assets without any further risks."
         }
         button={{
           text: "Continue",
@@ -72,7 +76,7 @@ export const RecoveryProcess = ({
     return (
       <CustomPortal
         title={"Switching Network"}
-        description={"Switch to personal Flashbot RPC network to prepare the transacion you will pay"}
+        description={"Allow us to create and switch to your personal RPC network to prepare the transaction bundle"}
         image={VideoSvg}
       />
     );
@@ -82,7 +86,7 @@ export const RecoveryProcess = ({
     return (
       <CustomPortal
         title={"Increase the gas"}
-        description={"Switch to personal Flashbot RPC network to prepare the transacion you will pay"}
+        description={"We recommend set a hight gas to ensure a successful asset recovery. Your increased gas allocation will facilitate the seamless completion of the transaction."}
         image={VideoSvg}
       />
     );
@@ -91,9 +95,9 @@ export const RecoveryProcess = ({
   if (recoveryStatus == RecoveryProcessStatus.SWITCH_TO_HACKED_ACCOUNT) {
     return (
       <CustomPortal
-        title={"Switch to hacked address"}
+        title={"Switch to Hacked Address"}
         description={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti."
+          "To proceed with asset recovery, switch to the compromised address in your wallet. This step is essential to verify ownership and continue with the recovery process."
         }
         button={{
           text: "Continue",
@@ -114,19 +118,20 @@ export const RecoveryProcess = ({
       <CustomPortal
         title={"Sign each transaction"}
         description={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti."
+          "Each bundled transaction requires your individual signature to verify and complete the asset recovery process. Please sign each transaction as they appear."
         }
         image={LogoSvg}
       />
     );
   }
 
+
   if (recoveryStatus == RecoveryProcessStatus.LISTEN_BUNDLE) {
     return (
       <CustomPortal
-        title={"Wait without refreshing the page"}
+        title={"Stay Patient, Stay Secure"}
         description={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio matt"
+          "Your asset recovery is in progress. To ensure a successful retrieval and accurate transaction completion stay on the page."
         }
         image={ClockSvg}
         indicator={blockCountdown}
@@ -139,10 +144,10 @@ export const RecoveryProcess = ({
       <CustomPortal
         title={"Your assets have been recovered!"}
         description={
-          "If we have helped you please, share the tool and if you want let us a tip to continue contributing"
+          "Check your safe wallet for your retrieved assets. Share your journey and consider support us with a tip to continue serving the crypto community."
         }
         button={{
-          text: "Continue",
+          text: "Finish",
           disabled: connectedAddress !== hackedAddress,
           action: () => finishProcess(),
         }}
